355e03fc222b61660aa9b59f2cf75c92f239d7bf
Ritik <rpzero.08@gmail.com>
2022-05-11 11:48:09 +0530

    save

---
+++ contracts/Contest.sol
@@ -0,0 +1,45 @@
+pragma solidity ^0.5.16;
+
+contract Contest {
+
+struct  Contestant  {
+    uint id;
+    string name;
+    uint voteCount;
+}
+mapping(uint => Contestant) public contestants;
+
+mapping (address => bool) public voters;
+
+uint public contestantsCount;
+constructor() public{
+     addContestant("Tom");
+    addContestant("Jerry");
+
+}
+
+
+
+function addContestant(string memory _name) private {
+contestantsCount ++;
+
+
+
+contestants[contestantsCount] = Contestant(contestantsCount,_name,0);
+
+
+}
+function vote(uint _contestantId) public {
+  
+   require(!voters[msg.sender]);
+   require(_contestantId > 0 && _contestantId <= contestantsCount);
+   
+    contestants[_contestantId].voteCount ++;
+
+    voters[msg.sender] = true;
+
+}
+
+
+}
+
